4608bc6c71ed61af86be874398b747130a816a8e
Indra-123456 <gantaindrasena123@gmail.com>
2024-11-15 09:04:39 +0530

    my third script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,8 +3,8 @@
 fruits=("mango" "apple" "pomo")
 
 
-echo "first fruit is : $fruits[0]"
+echo "first fruit is : ${fruits[0]}"
 
-echo "second fruit is : $fruits[1]"
+echo "second fruit is : ${fruits[1]}"
 
-echo "all fruits are : $fruits[@]"
+echo "all fruits are : ${fruits[@]}"
