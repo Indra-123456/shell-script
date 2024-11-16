111d17cf206cb3f3d5f4ff9d07497714e49f0b4e
Indra-123456 <gantaindrasena123@gmail.com>
2024-11-16 11:28:18 +0530

    my third script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,7 +5,20 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-  echo "he is a normal user, try install using root user"
-fi
+  echo "Error::he is a normal user, try install using root user"
+  exit 1;
+else 
+echo "he ia root user"
+
+yum install mysql -y
+
+if [ $? -ne 0]
+then
+   echo "error: mysql command need to be corrected"
+   exit 1;
+else
+   echo "success:: installtion of my sql is successfull"
+
+
 
       
