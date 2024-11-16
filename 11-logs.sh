3fbe9d0432eddad578e98090c78a40355d1d97c2
Indra-123456 <gantaindrasena123@gmail.com>
2024-11-16 12:53:39 +0530

    my third script

--- 11-logs.sh
+++ 11-logs.sh
@@ -6,13 +6,18 @@ timestamp=$(date +%F-%H-%M-%S)
 
 logfile="/tmp/$0-$timestamp.log"
 
+R="\e[31m"
+G="\e[32m"
+
+N="\e[0m"
+
 validate(){
     if [ $1 -ne 0 ]
     then
-        echo "error:  $2....failed"
+        echo "error:  $2.... $R failed $N"
         exit 1;
     else
-        echo "$2..... success"
+        echo "$2..... $G success $N"
     fi 
 }
 
@@ -25,7 +30,7 @@ else
 fi
 
 
-yum install -y mysql &>> $logfile
+yum install -y mysqlll &>> $logfile
 
 validate $? "Installing mysql"
 
